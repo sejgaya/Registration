e7daf8571a29bb2089cac28d7aa1b4f17fe0dce1
sejgaya <117446054+sejgaya@users.noreply.github.com>
2023-11-05 11:51:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './index.css'
+import './App.css';
+import Registration from './Registration'
+import React from 'react';
+
+
+function App() {
+  return (
+    
+    <div className='App'>
+     <Registration/>
+    </div>
+    
+  );
+}
+
+export default App;
